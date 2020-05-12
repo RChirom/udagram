50027c23c09104de9c8a94978f363a4d8cdbcdea
RChirom <rupamchirom@yahoo.com>
2020-05-12 09:40:24 +0530

    node module reinstall and local server test
    Test pass.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import express from 'express';
+import { Express, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import fs from 'fs';
+import { ESRCH } from 'constants';
 
 (async () => {
 
   // Init the Express application
-  const app = express();
+  const express = require('express');
+  const app: Express = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
@@ -13,27 +16,37 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
+  let image_url: string;
+
+  app.get("/filteredImage", async(req: Request, res: Response) => {
+    let {image_url} = req.query;
   //    1. validate the image_url query
+    if (!image_url){
+      res.status(400).send("Invalid Request! Query is missing");
+    }
+    
   //    2. call filterImageFromURL(image_url) to filter the image
+    filterImageFromURL(image_url).then((image) => {
   //    3. send the resulting file in the response
+      fs.readFile(image, (err, data) => {
+        if(err){
+          res.status(422).send("Processing error. Check the files")
+        }
+        res.writeHead(200, {'Content-Type':'image/jpeg'});
+        res.end(data);
   //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+        deleteLocalFiles(new Array(image));
+      });
+    }).catch(error => {
+      res.status(500).send("Error in returning path.")
+    });
+  });
 
-  /**************************************************************************** */
-
-  //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
